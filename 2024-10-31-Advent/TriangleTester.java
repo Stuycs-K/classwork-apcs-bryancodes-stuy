f54a37e9b490a5b41e3601a9d88d6c2be2f7e31b
Bryan Lam <bryanl156@nycstudents.net>
2024-11-04 10:54:10 -0500

    Add pair programming work for part b and revisions for part a

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -10,30 +10,46 @@ public class TriangleTester {
     return true;
   }
 
-  public static int countTrianglesA(String filename) {
-    int count = 0;
-    try {
-      File file = new File(filename);
-      Scanner input = new Scanner(file);
-      int[] sides = new int[3];
-      while(input.hasNext()){
-        for (int i = 0; i < 3; i++) {
-          String intString = input.next();
-          sides[i] = Integer.parseInt(intString);
+
+    public static int countTrianglesA(String filename) {
+      int count = 0;
+      try {
+        File file = new File(filename);
+        Scanner input = new Scanner(file);
+        while(input.hasNext()){
+          if (isTriangle(input.nextInt(), input.nextInt(), input.nextInt())) {
+            count++;
+          }
         }
-        if (isTriangle(sides[0], sides[1], sides[2])) {
-          count++;
-        }
+        input.close();
+      } catch (FileNotFoundException ex) {
+        System.out.println("File not found");
+        return 0;
       }
-      input.close();
-    } catch (FileNotFoundException ex) {
-      System.out.println("File not found");
-      return 0;
+      return count;
     }
-    return count;
-  }
+
+    public static int countTrianglesB(String filename) {
+      int count = 0;
+      try {
+        File file = new File(filename);
+        Scanner input = new Scanner(file);
+        int[] sides = new int[9];
+        while(input.hasNext()){
+          for (int i = 0; i < 9; i++) sides[i] = input.nextInt();
+          if (isTriangle(sides[0], sides[3], sides[6])) count++;
+          if (isTriangle(sides[1], sides[4], sides[7])) count++;
+          if (isTriangle(sides[2], sides[5], sides[8])) count++;
+        }
+        input.close();
+      } catch (FileNotFoundException ex) {
+        System.out.println("File not found");
+        return 0;
+      }
+      return count;
+    }
 
   public static void main(String[] args) {
-    System.out.println(countTrianglesA("inputTri.txt"));
+    System.out.println(countTrianglesB("inputTri.txt"));
   }
 }
