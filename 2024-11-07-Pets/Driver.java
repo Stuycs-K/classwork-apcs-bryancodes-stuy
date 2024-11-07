591f22c010ee952cbae0bc45b5d497af93b49067
Bryan Lam <bryanl156@nycstudents.net>
2024-11-07 12:36:41 -0500

    Used speak() method for animal+bird pairs that worked

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,7 +1,7 @@
 // The speak() method for Bird printed "My name is The Mighty Duck"
 // instead of "My name is Duck"
 // And the speak() method for Animal printed the same thing
-// b2 does not work because it's a Bird object and is calling Animal
+// b2 does not work because it's a Bird type and is assigned as an Animal 
 // from the super class, which is not possible, as Animal does not have all
 // the necessary functions for bird, since it doesn't have height or color.
 public class Driver{
@@ -17,8 +17,11 @@ public class Driver{
     System.out.println("Returned:");
     myAnimal.speak();
     Animal a1 = new Animal("Moo", 46, "Cow");
-    Bird b1 = new Bird("Ahh", 7, "Eagle", 18, "Brown");
+    Bird b1 = new Bird("Ahh", 7, "Eagle", 18.0, "Brown");
     // Bird b2 = new Animal("Bark", 16, "Dog");
-    Animal a2 = new Bird("Chirp", 7, "Robin", 3, "Red");
+    Animal a2 = new Bird("Chirp", 7, "Robin", 3.0, "Red");
+    a1.speak();
+    b1.speak();
+    a2.speak();
   }
 }
