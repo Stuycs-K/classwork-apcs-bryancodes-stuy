dcd0a523d36bb2ec7ac884924dba263d294589b0
Bryan Lam <bryanl156@nycstudents.net>
2024-11-07 10:39:28 -0500

    Tested speak()

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,5 +1,9 @@
 public class Driver{
   public static void main(String[] args){
-    
+    Animal myAnimal = new Animal("Quack", 10, "Duck");
+    System.out.println("Expected:");
+    System.out.println("My name is Duck.\nI am 10 years old.\nI say Quack.");
+    System.out.println("Returned:");
+    myAnimal.speak();
   }
 }
