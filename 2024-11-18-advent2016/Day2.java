25d74b124c9b0d120feebab30b2e8d221cc12736
Bryan Lam <bryanl156@nycstudents.net>
2024-11-20 11:01:03 -0500

    Add error message and add pad array

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 
 public class Day2{
     public static int bathroom(String filename){
-        File file = new File(filename);
-        Scanner input = new Scanner(file);
-        int button = 5;
-        while (input.hasNextLine()){
-            while (input.hasNext()){
-                
+        try{    
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            int[][] pad = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+            int r = 1;
+            int c = 1;
+            while (input.hasNextLine()){
+                for (int i = 0; i < )
             }
+        }catch (FileNotFoundException ex){
+            System.out.println("File not found");
+            return 0;
         }
     }
 }
