268a587006daa79866df34da65646733005c83e3
Bryan Lam <bryanl156@nycstudents.net>
2024-11-19 19:56:33 -0500

    Start days 2, 4, and 6

---
+++ 2024-11-18-advent2016/Day2.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day2{
+    public static int bathroom(String filename){
+        File file = new File(filename);
+        Scanner input = new Scanner(file);
+        int button = 5;
+        while (input.hasNextLine()){
+            while (input.hasNext()){
+                
+            }
+        }
+    }
+}
