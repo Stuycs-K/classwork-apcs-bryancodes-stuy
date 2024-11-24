73051ebe33d9c387de11a4b24328c881b0547c2c
Bryan Lam <bryanl156@nycstudents.net>
2024-11-24 14:41:59 -0500

    Rearranged input for Day 6

--- 2024-11-18-advent2016/Day6.java
+++ 2024-11-18-advent2016/Day6.java
@@ -3,5 +3,36 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Day6{
-    
+    public static String signal(String filename){
+        try{
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            String message = "";
+            int line = 0;
+            while (input.hasNextLine()){
+                line++;
+            }
+            String[][] comms = new String[line][];
+            int i = 0;
+            String each = "";
+            while (input.hasNextLine()){
+                each = input.nextLine();
+                coms[i][] = input.split("");
+                i++;
+            }
+            String[][] rearrange = new String[coms[i][0].length][line];
+            for (int j = 0; j < rearrange.length; j++){
+                for (int k = 0; k < rearrange[j].length; k++){
+                    rearrange[j][k] = comms[k][j];
+                }
+            }
+        }catch (FileNotFoundException ex){
+            System.out.println("File not found");
+            return "";
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(signal("input.txt"));
+    }
 }
