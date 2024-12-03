c1d50619fa9faf0c3f167f3053cb64a655fd5e28
Bryan Lam <bryanl156@nycstudents.net>
2024-12-03 11:16:45 -0500

    Create file and add color method

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,8 @@
+public class ColorDemo{
+  public static void color(int font; int back){
+    System.out.print("\u001b[" + font + ";" + back + "m");
+  }
+  public static void main(String[] args){
+
+  }
+}
