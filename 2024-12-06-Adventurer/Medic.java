05709f4af94f7b78600ddc1868830ca82528bfd6
Bryan Lam <bryanl156@nycstudents.net>
2024-12-06 10:59:42 -0500

    Create specialName, added to constructors, and wrote getSpecialName and getSpecial

--- 2024-12-06-Adventurer/Medic.java
+++ 2024-12-06-Adventurer/Medic.java
@@ -1,14 +1,25 @@
 public class Medic extends Adventurer{
 
   private int medicalKit;
+  private String specialName;
 
-  public Medic(String name, int supply){
+  public Medic(String name, int supply, int code){
     super(name);
     medicalKit = supply;
+    specialName = "Medic " + code;
   }
 
-  public Medic(String name, int hp){
+  public Medic(String name, int hp, int supply, int code){
     super(name, hp);
     medicalKit = supply;
+    specialName = "Medic " + code;
+  }
+
+  public String getSpecialName(){
+    return specialName;
+  }
+
+  public int getSpecial(){
+    return medicalKit;
   }
 }
