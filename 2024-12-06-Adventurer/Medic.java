2e1aafea3babd8d1b8d0c54eaa32ab5d4608738c
Bryan Lam <bryanl156@nycstudents.net>
2024-12-07 13:31:51 -0500

    Add maxMeds and created setSpecial and getSpecialMax

--- 2024-12-06-Adventurer/Medic.java
+++ 2024-12-06-Adventurer/Medic.java
@@ -1,17 +1,19 @@
 public class Medic extends Adventurer{
 
-  private int medicalKit;
+  private int medicalKit, maxMeds;
   private String specialName;
 
   public Medic(String name, int supply, int code){
     super(name);
     medicalKit = supply;
+    maxMeds = supply + 5;
     specialName = "Medic " + code;
   }
 
   public Medic(String name, int hp, int supply, int code){
     super(name, hp);
     medicalKit = supply;
+    maxMeds = supply + 5;
     specialName = "Medic " + code;
   }
 
@@ -21,5 +23,13 @@ public class Medic extends Adventurer{
 
   public int getSpecial(){
     return medicalKit;
+  }
+
+  public void setSpecial(int n){
+    medicalKit = n;
+  }
+
+  public int getSpecialMax(){
+    return maxMeds;
   }
 }
