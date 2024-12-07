889fa0931b96a424123d3379a92f5cc2661796cf
Bryan Lam <bryanl156@nycstudents.net>
2024-12-07 14:01:05 -0500

    Write attack and support other adventurer

--- 2024-12-06-Adventurer/Medic.java
+++ 2024-12-06-Adventurer/Medic.java
@@ -32,4 +32,16 @@ public class Medic extends Adventurer{
   public int getSpecialMax(){
     return maxMeds;
   }
+
+  public String attack(Adventurer other){
+    super.other.setHP(super.other.getHP / 2); 
+    setSpecial(n - 1); // The medical kits also contain poisons or make a person overdose.
+  }
+
+  public String support(Adventurer other){
+    int newHP = super.other.getHP * 2;
+    if (newHP > super.other.getmaxHP) newHP = super.other.getmaxHP;
+    super.other.setHP(newHP);
+    setSpecial(n - 1); // Use medicine on the other adventurer. 
+  }
 }
