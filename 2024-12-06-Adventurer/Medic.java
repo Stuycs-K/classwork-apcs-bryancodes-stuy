a4f383899cb1d3d3d2482fef1df249deec6705a6
Bryan Lam <bryanl156@nycstudents.net>
2024-12-08 12:18:54 -0500

    Write support() and specialAttack

--- 2024-12-06-Adventurer/Medic.java
+++ 2024-12-06-Adventurer/Medic.java
@@ -34,14 +34,28 @@ public class Medic extends Adventurer{
   }
 
   public String attack(Adventurer other){
-    super.other.setHP(super.other.getHP / 2); 
+    super.other.applyDamage(super.other.getHP / 2); 
     setSpecial(n - 1); // The medical kits also contain poisons or make a person overdose.
   }
 
   public String support(Adventurer other){
-    int newHP = super.other.getHP * 2;
-    if (newHP > super.other.getmaxHP) newHP = super.other.getmaxHP;
+    int newHP = super.other.getHP() * 2;
+    if (newHP > super.other.getmaxHP()) newHP = super.other.getmaxHP();
     super.other.setHP(newHP);
     setSpecial(n - 1); // Use medicine on the other adventurer. 
+  }
+
+  public String support(){
+    int newHP = super.getHP() * 2;
+    if (newHP > super.getmaxHP()) newHP = super.getmaxHP();
+    super.setHP(newHP);
+    setSpecial(n - 1);
+  }
+
+  public String specialAttack(Adventurer other){
+    int otherHealth = super.other.getHP();
+    super.other.applyDamage(otherHealth); //kills opponent completely
+    setSpecial(n - 3); // Uses three medical kits
+    setSpecial(otherHealth * 3); // Each hp can get 3 medical kits
   }
 }
