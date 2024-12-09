0a391c0f2da2d5ccc311d265df51b0e008cee010
Bryan Lam <bryanl156@nycstudents.net>
2024-12-09 12:28:33 -0500

    Create for loop and if statements in try

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -5,9 +5,46 @@ public class Game{
     Scanner userInput = new Scanner(System.in);
     System.out.println("Enter username");
     String userName = userInput.nextLine();
-    CodeWarrior player = new CodeWarrior(userName);
-    Medic enemy = new Medic("Bryan", 8, "bot");
+    Adventurer player = new CodeWarrior(userName);
+    Adventurer enemy = new Medic("Bryan", 8, "bot");
     System.out.println(player.getName() + ", " + player.getHP() + "/" + player.getmaxHP() + " HP, " + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecial());
     System.out.println(enemy.getName() + ", " + enemy.getHP() + "/" + enemy.getmaxHP() + " HP, " + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecial());
+    while (player.getHP() > 0 && enemy.getHP()){
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+      String move = userInput.nextLine();
+      try{
+        if (move.equals("a") || move.equals("attack")){
+          System.out.println(player.attack(enemy));
+          int choose = (int)(Math.random() * 10);
+          if (choose > 5){
+            System.out.println(enemy.support());
+          }else{
+            System.out.println(enemy.attack(player));
+          }
+        }
+        if (move.equals("sp") || move.equals("special")){ 
+          System.out.println(player.specialAttack(enemy));
+          int choose = (int)(Math.random() * 10);
+          if (choose > 5){
+            System.out.println(enemy.support());
+          }else{
+            System.out.println(enemy.attack(player));
+          }
+        }
+        if (move.equals("su") || move.equals("support")){
+          System.out.println(player.support());
+          int choose = (int)(Math.random() * 10);
+          if (choose > 5){
+            System.out.println(enemy.support());
+          }else{
+            System.out.println(enemy.attack(player));
+          }
+        }
+        if (move.equals("quit")) {
+          System.out.println("The game is over.");
+          System.exit(0);
+        }
+      }
+    }
   }
 }
