f573fd59d285a8636bcc3403186a56ef6464655b
Bryan Lam <bryanl156@nycstudents.net>
2025-01-02 10:54:17 -0500

    Write else statement and tested 0 - 2

--- 2025-01-02/fib.java
+++ 2025-01-02/fib.java
@@ -3,7 +3,13 @@ public class fib{
     if (n == 0 || n == 1){
       return n;
     }else{
-      
+      return fib(n - 1) + fib(n - 2);
     }
+  }
+
+  public static void main(String[] args){
+    System.out.println(fib(0));
+    System.out.println(fib(1));
+    System.out.println(fib(2));
   }
 }
