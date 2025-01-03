cd6e9a85dfed10ecd7a0a0c35f41270c8fd24e85
Bryan Lam <bryanl156@nycstudents.net>
2025-01-03 11:02:14 -0500

    Modify result

--- 2025-01-02/Word.java
+++ 2025-01-02/Word.java
@@ -3,6 +3,12 @@ public class Word{
     for (int i = 0; i < alphabet.length(); i++){
       if (remainingLetters == 0){
         System.out.println(result);
+      }else{
+        if (i == alphabet.length() - 1){
+          result += alphabet.substring(i);
+        }else{
+          result += alphabet.substring(i, i + 1);
+        }
       }
   }
 }
